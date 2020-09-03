38eadcd325e448cc57dc9beeaf7de931f8481b0b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-03 22:36:55 +0530

    fixed2

--- asyncActions.js
+++ asyncActions.js
@@ -1,84 +1,86 @@
-const redux = require('redux');
-const createStore = redux.createStore
-const applyMiddleware = redux.applyMiddleware
-const thunkMiddleware = require('redux-thunk').default
-const axios = require('axios');
+const redux = require("redux");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
 
 // state
 const initialState = {
-    loading: false,
-    users: [],
-    error: ''
-}
+  loading: false,
+  users: [],
+  error: "",
+};
 
 // actions
-const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST'
-const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS'
-const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
 
 // action creator
 const fetchUsersRequest = () => {
-   return {
-       type: FETCH_USERS_REQUEST
-   }
-}
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+};
 
-const fetchUsersSuccess = users => {
-   return {
-       type: FETCH_USERS_SUCCESS,
-       payload: users
-   }
-}
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
 
-const fetchUsersFailure = error => {
-   return {
-       type: FETCH_USERS_FAILURE,
-       payload: error
-   }
-}
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+};
 
 // reducer
 const reducer = (state = initialState, action) => {
-   switch(action.type){
-       case FETCH_USERS_REQUEST:
-           return {
-               ...state,
-               loading: true
-            //    yahan pe pure state ko copy kia h or sirf loading ko true set kr rhe h
-           }
-       case FETCH_USERS_SUCCESS:
-           return {
-               loading: false,
-               users: action.payload,
-               error: ''
-           }
-       case FETCH_USERS_FAILURE:
-           return {
-               loading: false,
-               users: [],
-               error: action.payload
-           }
-   }
-}
-
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        //    yahan pe pure state ko copy kia h or sirf loading ko true set kr rhe h
+      };
+    case FETCH_USERS_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+  }
+};
 
 // actions creators returns an action but what the thunk middleware brings to the table is the ability for the action creator to return a function instead of an action object so we can now return a function
 const fetchUsers = () => {
-    return function(dispatch) {
-        dispatch(fetchUsersRequest())
-        axios.get('https://jsonplaceholder.typicode.com/users')
-        .then(response => {
-            // response.data is the array of users
-            const users = response.data.map(user => user.id)
-            dispatch(fetchUsersSuccess(users))
-        })
-        .catch(error => {
-            // error.message in the error description
-            dispatch(fetchUsersFailure(error.message))
-        })
-    }
-}
+  return function (dispatch) {
+    dispatch(fetchUsersRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        // response.data is the array of users
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        // error.message in the error description
+        dispatch(fetchUsersFailure(error.message));
+      });
+  };
+};
 
-const store = createStore(reducer, applyMiddleware(thunkMiddleware))
-store.subscribe(() => { console.log(store.getState()) })
-store.dispatch(fetchUsers())
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(fetchUsers());
