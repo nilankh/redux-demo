bdf8a4b01824614a3b358dd456d49d66c13eee8d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-03 22:36:39 +0530

    fixed2

--- index3.js
+++ index3.js
@@ -1,7 +1,7 @@
 // Learning about multiple reducers
 const redux = require("redux");
 const createStore = redux.createStore; //created store
-const combineReducers = redux.combineReducers
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -25,8 +25,6 @@ const initialCakeState = {
 const initialIceCreamState = {
   numOfIceCreams: 20,
 };
-
-
 
 const cakereducer = (state = initialCakeState, action) => {
   switch (action.type) {
@@ -55,9 +53,9 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
 
 // the convention is to call the combination of all your reducer as the root reducer(this method accepts an object each key/value pair for this object corresponds to a reducer, for the cake reducers i will specify cake as key and for incecream reducers will specify icecream as key)
 const rootReducer = combineReducers({
-    cake: cakereducer,
-    iceCream: iceCreamReducer
-})
+  cake: cakereducer,
+  iceCream: iceCreamReducer,
+});
 const store = createStore(rootReducer);
 // const store = createStore(reducer);
 // see ye create store, there is a problem, the create store method accepts only one reducer, so how do we let redux know about both the reducer{{{{React provides a method that combine reducers to  combine multiple reducers into a single reducer which can be passed to create store method(created at line4)}}}}
